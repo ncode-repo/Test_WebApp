6146964f5d0d1708533b0d772a7451c758a2963c
nilesh2013 <nilesh.year13@gmail.com>
2013-02-08 14:47:21 +0530

    TEsting

--- TestGit/src/Temp.java
+++ TestGit/src/Temp.java
@@ -3,7 +3,8 @@
 public class Temp {
 	
 	public static void main(String[] args) {   
-		System.out.println("Testing Test-1 GIT repository from Eclipse Helios... Testing Test-1 GIT repository from Eclipse Helios...  ");
+		System.out.println("Testing Test-1 GIT repository from Eclipse Helios...");
+		System.out.println("Testing Test-1 GIT repository from Eclipse Helios...");
 	}
 	
 }
