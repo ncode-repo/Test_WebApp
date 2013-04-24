809375d8b7f5cf452e25c5a3faccdb13e88d4eee
nilesh2013 <nilesh.year13@gmail.com>
2013-04-24 10:10:23 +0530

    Commiting in local- 1st time

--- TestGit/src/Temp.java
+++ TestGit/src/Temp.java
@@ -4,7 +4,7 @@ public class Temp {
 	
 	public static void main(String[] args) {   
 		System.out.println("Testing Test-1 GIT repository from Eclipse Helios...");
-		System.out.println("Testing Test-1 GIT repository from Eclipse Helios...");
+		System.out.println("Testing Test-1 GIT repository from Eclipse Helios...");  
 	}
 	
 }
---
+++ Test_WebApp/src/Temp.java
@@ -0,0 +1,10 @@
+
+/** * Generated class, please do not edit. */
+public class Temp {
+	
+	public static void main(String[] args) {
+		System.out.println("Testing Test-1 GIT repository from Eclipse Helios");
+		System.out.println("Testing Test-1 GIT repository from Eclipse Helios");
+	}
+	
+}
